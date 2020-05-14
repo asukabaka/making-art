ba04293227ec74e5e7b93bc3e24af8581ef10f46
asukabaka <48495833+asukabaka@users.noreply.github.com>
2020-05-13 18:10:07 -0700

    s

--- sketch.js
+++ sketch.js
@@ -18,7 +18,10 @@ let font,
     fontsize = 80;
 let snowflakes = []; // array to hold snowflake objects
 let c;
+var mic, soundFile;
+var amplitude;
 
+var prevLevels = new Array(60);
 var song;
 var sound;
 
@@ -36,6 +39,21 @@ function preload() {
 
 
 function setup() {
+  c = createCanvas(windowWidth, windowHeight);
+  background(0);
+  noStroke();
+
+  rectMode(CENTER);
+
+  mic = new p5.AudioIn();
+  mic.start();
+
+  // load the sound, but don't play it yet
+  soundFile = loadSound('song.mp3')
+
+  amplitude = new p5.Amplitude();
+  amplitude.setInput(mic);
+  amplitude.smooth(0.6);
   music.play();
   s = color(mouseX/2, 0, width/2, 0, 1.0);
   r = color(50, mouseY/2, 50, height/2, 1.0);
@@ -107,6 +125,37 @@ function draw() {
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
   pop();
+
+  var level = amplitude.getLevel();
+
+  // rectangle variables
+  var spacing = 10;
+  var w = width/ (prevLevels.length * spacing);
+
+  var minHeight = 2;
+  var roundness = 20;
+
+  // add new level to end of array
+  prevLevels.push(level);
+
+  // remove first item in array
+  prevLevels.splice(0, 1);
+
+  // loop through all the previous levels
+  for (var i = 0; i < prevLevels.length; i++) {
+
+    var x = map(i, prevLevels.length, 0, width/2, width);
+    var h = map(prevLevels[i], 0, 0.5, minHeight, height);
+
+    var alphaValue = log(map(i, 0, prevLevels.length, 1, 250));
+
+    var hueValue = map(h, minHeight, height, 200, 255);
+
+    fill(hueValue, 255, 255, alphaValue);
+
+    rect(x, height/2, w, h);
+    rect(width - x, height/2, w, h);
+  }
 
 }
 
